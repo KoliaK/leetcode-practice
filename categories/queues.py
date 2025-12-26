d90c71636687cc0252441bfe46db3f873886e7ae
KoliaK <xkoliakx@gmail.com>
2025-12-26 14:30:05 -0300

    solved: number of recent calls

--- categories/queues.py
+++ categories/queues.py
@@ -131,10 +131,77 @@ class MyStack:
         # or return len(self.queue) == 0
         return not self.queue
 
-my_stack = MyStack()
-my_stack.void_push(1)
-my_stack.void_push(2)
-my_stack.void_push(3)
-print(my_stack.int_pop()) # -> expected to return 3!
-print(my_stack.int_top()) # -> expected to return 2 (3 has been poped)!
-print(my_stack.boolean_empty()) # -> expected to return False
+# my_stack = MyStack()
+# my_stack.void_push(1)
+# my_stack.void_push(2)
+# my_stack.void_push(3)
+# print(my_stack.int_pop()) # -> expected to return 3!
+# print(my_stack.int_top()) # -> expected to return 2 (3 has been poped)!
+# print(my_stack.boolean_empty()) # -> expected to return False
+
+# NUMBER OF RECENT CALLS
+'''
+You have a RecentCounter class which counts the number of 
+recent requests within a certain time frame.
+
+Requirements: 
+Implement the RecentCounter class:
+
+RecentCounter(): 
+Initializes the counter with zero requests.
+
+int ping(int t): 
+Adds a new request at time t (milliseconds). Returns the number of requests that have happened in the past 3000 milliseconds (including the new request).
+
+Constraint:
+It is guaranteed that every call to ping uses a strictly larger value of t than the previous call (time always moves forward).
+
+Example:
+counter = RecentCounter()
+counter.ping(1)    # requests: [1], range: [-2999, 1], return: 1
+counter.ping(100)  # requests: [1, 100], range: [-2900, 100], return: 2
+counter.ping(3001) # requests: [1, 100, 3001], range: [1, 3001], return: 3
+counter.ping(3002) # requests: [1, 100, 3001, 3002], range: [2, 3002], return: 3
+                   # ^ Note: '1' is now older than 3000ms ago
+                   # (3002 - 3000 = 2), so '1' drops off.
+
+How would you use a Queue here?
+When a new ping comes in, you add it.
+Then, what do you check to decide what to remove?
+(Think about the MovingAverage problem, but instead of checking len(queue) > size, what is your condition for removal?)
+'''
+
+class RecentCounter:
+    def __init__(self):
+        self.requests = deque() # this will receive the ping requests
+        self.WINDOW_SIZE = 3000 # time limit in ms
+
+    def ping(self, t : int) -> int:
+        # this method returns the number of requests that
+        # have happened in the past 3000 milliseconds
+        # (including the new request)
+
+        # time of request must be stored in self.requests
+        # like [1, 15, 100] (116 ms used from the ms_window)
+        self.requests.append(t)
+
+        # loop logic
+        # at self.requests[0] = 1
+        # 1 < (1 - 3000) => 1 < -2999 ? False
+        # 1 < (100 - 3000) => 1 < -2900 ? False
+        # 1 < (3001 - 3000) => 1 < 1 ? False
+        # 1 < (3002 - 3000) => 1 < 2 ? True
+        # REMOVE from the queue so now [100, 3001, 3002]
+        while self.requests[0] < (t - self.WINDOW_SIZE):
+            self.requests.popleft()
+        # return how many requests were made in the past 3000ms
+        return len(self.requests)
+
+counter = RecentCounter()
+print(counter.ping(1))    # requests: [1], range: [-2999, 1], return: 1
+print(counter.ping(100))  # requests: [1, 100], range: [-2900, 100], return: 2
+print(counter.ping(3001)) # requests: [1, 100, 3001], range: [1, 3001], return: 3
+print(counter.ping(3002)) # requests: [1, 100, 3001, 3002], range: [2, 3002], return: 3
+                   # ^ Note: '1' is now older than 3000ms ago
+                   # (3002 - 3000 = 2), so '1' drops off.
+            
