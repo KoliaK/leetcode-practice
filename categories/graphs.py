ca7bb2350d9c1499beee6ef59ca26965741d19c6
KoliaK <xkoliakx@gmail.com>
2026-01-02 09:21:09 -0300

    commented senior approach of number of islands problem

--- categories/graphs.py
+++ categories/graphs.py
@@ -92,32 +92,51 @@ class IslandCounter:
 # SENIOR APPROACH WITH SINKING PATTERN (BY GEMINI)
 class Solution:
     def numIslands(self, grid: list[list[str]]) -> int:
+        # if grid is empty
         if not grid: return 0
         
+        # tuple unpack, if 4x3 grid -> 4 rows, 3 cols
         rows, cols = len(grid), len(grid[0])
+
+        # island counter
         islands = 0
         
         # 1. The Scanner
         for r in range(rows):
             for c in range(cols):
+                # if cell is a land spot
                 if grid[r][c] == '1':
+                    # increment island counter
                     islands += 1
+                    # call the spreader
                     self.sink_island(grid, r, c, rows, cols) 
-                    # Call the spreader
-        
+                    
+        # return island counter
         return islands
 
     # 2. The Spreader (DFS Version - often shorter to write than BFS)
+    # DFS = DEPTH-FIRST SEARCH
+    # BFS = BREADTH-FIRST SEARCH
     def sink_island(self, grid, r, c, rows, cols):
         # Base Case: If we go out of bounds OR hit water ('0'), stop.
+        # if row or column are negative or current cell is 'water'
         if not (0 <= r < rows and 0 <= c < cols) or grid[r][c] == '0':
             return
+        
+        # it can also be written as
+        # if not (rows > r >= 0 and cols > c >= 0) or grid[r][c] == '0':
+            
         
         # SINK IT! Mark as visited by changing '1' to '0'
         grid[r][c] = '0'
         
         # Recursively sink neighbors (Up, Down, Left, Right)
-        self.sink_island(grid, r + 1, c, rows, cols)
-        self.sink_island(grid, r - 1, c, rows, cols)
-        self.sink_island(grid, r, c + 1, rows, cols)
-        self.sink_island(grid, r, c - 1, rows, cols)
+        # (0, 0) = TOP-LEFT corner of the grid
+        # r += 1 as you go DOWN
+        # c += 1 as you go RIGHT
+        self.sink_island(grid, r + 1, c, rows, cols) # down
+        self.sink_island(grid, r - 1, c, rows, cols) # up
+        self.sink_island(grid, r, c + 1, rows, cols) # right
+        self.sink_island(grid, r, c - 1, rows, cols) # left
+
+
